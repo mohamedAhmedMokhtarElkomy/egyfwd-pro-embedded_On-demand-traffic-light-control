33b23e2c11b2fa7dac41aeacd32f9642836b64b8
mohamedAhmedMokhtarElkomy <84991746+mohamedAhmedMokhtarElkomy@users.noreply.github.com>
2022-09-12 10:40:12 -0700

    create project

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * On-demand-traffic-light-control.c
+ *
+ * Created: 9/12/2022 10:37:56 AM
+ * Author : admin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
