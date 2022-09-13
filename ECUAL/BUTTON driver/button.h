abe89f18a6c50997818d5e1afcc6563b1f529915
mohamedAhmedMokhtarElkomy <84991746+mohamedAhmedMokhtarElkomy@users.noreply.github.com>
2022-09-12 18:01:18 -0700

    button driver

--- ECUAL/BUTTON driver/button.h
+++ ECUAL/BUTTON driver/button.h
@@ -9,7 +9,24 @@
 #ifndef BUTTON_H_
 #define BUTTON_H_
 
+#include "../../MCAL/DIO/dio.h"	//Upper layer include lower layer
 
+
+#define PEDESTRIAN_BUTTON_PORT PORT_C
+#define PEDESTRIAN_BUTTON_PIN 0
+
+typedef enum EN_BUTTON_STATUS
+{
+	BUTTON_INIT_OK,
+	BUTTON_INIT_FAILED,
+	
+	BUTTON_READ_OK,
+	BUTTON_READ_FAILED,
+	
+}EN_BUTTON_STATUS;
+
+EN_BUTTON_STATUS BUTTON_init(uint8_t buttonPort, uint8_t buttonPin);
+EN_BUTTON_STATUS BUTTON_read(uint8_t buttonPort, uint8_t buttonPin, uint8_t *value);
 
 
 
